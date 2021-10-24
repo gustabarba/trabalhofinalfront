e93fa1e1b1e3996c25ccf4bdd2fbd64904a778dd
gustavo <gusta@barba>
2021-10-24 17:49:52 -0300

    Pequenas mudanças insignificantes.

--- js/mostrarCliente.js
+++ js/mostrarCliente.js
@@ -22,52 +22,62 @@ function imprimirUsuario(cliente) {
     enderecoSemRua = encodeURI(enderecoSemRua);
     let impressao = "";
     impressao += `
-    <div class="cliente card col-12" >
-        <div class="row g-0">
-            <div class="container-foto col-12 col-sm-12 col-md-4 col-xl-3">
-            <span class="img-shadow">
-                <img src="${
-                    cliente.picture.large
-                }" class="foto-cliente img-fluid" alt="...">
-                </span>
-            </div>
-            
+        <div class="cliente card col-12">
+            <div class="row g-0">
+                <div class="container-foto col-12 col-sm-12 col-md-4 col-xl-3">
+                    <span class="img-shadow">
+                        <img
+                            src="${cliente.picture.large}"
+                            class="foto-cliente img-fluid"
+                            alt="..."
+                        />
+                    </span>
+                </div>
+
                 <div class="col-12 col-sm-12 col-md-4 col-xl-3 card-body col-12">
                     <h5 class="card-title">
-                        <b>
-                            ${cliente.name.first} ${cliente.name.last}
-                        </b>
+                        <b> ${cliente.name.first} ${cliente.name.last} </b>
                     </h5>
                     <p class="card-text">
-                    <b>Idade:</b> ${cliente.dob.age}<br>
-                    <b>Gênero:</b> ${
-                        cliente.gender == "female" ? "feminino" : "masculino"
-                    }<br>
-                    <b>Email:</b> ${cliente.email}<br>
-                    <b>Telefone:</b> ${cliente.phone}<br>
+                        <b>Idade:</b> ${cliente.dob.age}<br />
+                        <b>Gênero:</b> ${
+                            cliente.gender == "female"
+                                ? "feminino"
+                                : "masculino"
+                        }<br />
+                        <b>Email:</b> ${cliente.email}<br />
+                        <b>Telefone:</b> ${cliente.phone}<br />
                     </p>
                 </div>
-            
 
-            <div class="col-12">
-                <div class="card-body">
-                <p class="card-text">
-                    
-                    <b>Endereço:</b> 
-                    ${cliente.location.street.name} 
-                    ${cliente.location.street.number}, 
-                    ${cliente.location.city}, 
-                    ${cliente.location.state} – 
-                    ${cliente.location.country}<br>
-                </p>
-                
+                <div class="col-12">
+                    <div class="card-body">
+                        <p class="card-text">
+                            <b>Endereço:</b>
+                            ${cliente.location.street.name}
+                            ${cliente.location.street.number}, 
+                            ${cliente.location.city},
+                            ${cliente.location.state} – 
+                            ${cliente.location.country}
+                        </p>
+                    </div>
+                </div>
+                <div class="card-img-bottom col-12">
+                    <div style="width: 100%">
+                        <iframe
+                            width="100%"
+                            height="300"
+                            frameborder="0"
+                            scrolling="no"
+                            marginheight="0"
+                            marginwidth="0"
+                            style="border-radius: 5px; background-color: #ddd"
+                            src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=${enderecoSemRua}&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+                        ></iframe>
+                    </div>
                 </div>
             </div>
-            <div class="card-img-bottom col-12">
-                <div style="width: 100%"><iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" style="border-radius:5px; background-color:#ddd" src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=${enderecoSemRua}&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
-            </div>
         </div>
-    </div> 
     `;
     container.innerHTML = impressao;
 }
--- js/mostrarclientes.js
+++ js/mostrarclientes.js
@@ -23,37 +23,43 @@ function obterClientes(num, callback) {
 function imprimirClientes(resultado) {
     let container = document.querySelector("#lista-clientes");
     let impressao = "";
-
     resultado.results.forEach((cliente) => {
+        let hashCliente = encodeURI(
+            CryptoJS.AES.encrypt(JSON.stringify(cliente), "1234").toString()
+        );
         impressao += `
-        <div class="cliente card col-12" >
-            <div class="row g-0">
-                <div class="container-foto col-12 col-sm-12 col-md-4 col-xl-3">
-                <span class="img-shadow">
-                    <img src="${
-                        cliente.picture.large
-                    }" class="foto-cliente img-fluid" alt="...">
-                    </span>
-                </div>
-                <div class="col-12 col-sm-12 col-md-8 col-xl-9">
-                    <div class="card-body">
-                    <h5 class="card-title">${cliente.name.first} ${
-            cliente.name.last
-        }</h5>
-                    <p class="card-text">${
-                        cliente.location.timezone.description
-                    }</p>
-                    <p class="card-text">
-                        <a href="./cliente.html?u=${CryptoJS.AES.encrypt(
-                            JSON.stringify(cliente),
-                            "1234"
-                        ).toString()}" class="tema-base btn btn-primary btn-sm" tabindex="-1" role="button">Ver cliente
-                        </a>                             
-                    </p>
+            <div class="cliente card col-12">
+                <div class="row g-0">
+                    <div class="container-foto col-12 col-sm-12 col-md-4 col-xl-3">
+                        <span class="img-shadow">
+                            <img
+                                src="${cliente.picture.large}"
+                                class="foto-cliente img-fluid"
+                                alt="..."
+                            />
+                        </span>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-8 col-xl-9">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                ${cliente.name.first} ${cliente.name.last}
+                            </h5>
+                            <p class="card-text">
+                                ${cliente.location.timezone.description}
+                            </p>
+                            <p class="card-text">
+                                <a
+                                    href="./cliente.html?u=${hashCliente}"
+                                    class="tema-base btn btn-primary btn-sm"
+                                    tabindex="-1"
+                                    role="button"
+                                    >Ver cliente
+                                </a>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div> 
         `;
     });
     container.innerHTML = impressao;
